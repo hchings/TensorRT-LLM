4c12852d8a5aee459d69a887f5182595ac868ba8
Erin Ho <14718778+hchings@users.noreply.github.com>
2025-11-16 14:44:36 +0000

    placement, asyncllm, and basic tests

--- tensorrt_llm/executor/ray_gpu_worker.py
+++ tensorrt_llm/executor/ray_gpu_worker.py
@@ -1,3 +1,4 @@
+import gc
 import importlib
 import os
 from pathlib import Path
@@ -216,6 +217,8 @@ class RayGPUWorker(RpcWorkerMixin, BaseWorker):
             torch.cuda.synchronize()
             release_with_tag(*tags)
             torch.cuda.synchronize()
+            gc.collect()
+            torch.cuda.empty_cache()
         except Exception as e:
             logger.error(f"Encountered an error in sleep: {e}")
             raise e
